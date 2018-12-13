02484d004f0bc35d8cb6ac67a02561b39822fd1a
Kyle Hong <kylehong@rocketmail.com>
2018-12-13 12:09:32 -0700

    js challenge 1

--- answer.js
+++ answer.js
@@ -1,1 +1,11 @@
 // *** YOUR ANSWER BELOW ***
+function bottlesSong(n){
+  let str = "";
+    while(n>0){
+      let num = n-1;
+ str += `${n} bottles of beer on the wall. ${n} bottles of beer. You take one down, pass it around. ${num} bottles of beer.`;
+ n--;
+  }
+    return  str;
+}
+  bottlesSong(10);
